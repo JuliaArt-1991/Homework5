95f66c41fa2d92362ae2ccd30d2c32191202fca7
Julia <artamon-julia@ya.ru>
2023-01-30 22:16:23 +0700

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// task 36
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+}
+
+void ReleaseArray(int[] array)
+{
+    int summa = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        summa += array[i];
+    }
+    Console.WriteLine($"Сумма элементов на нечетных позициях: {summa}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
